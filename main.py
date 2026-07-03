e9f2d3d0839a2e41c915bc9f44b5421515f68e3a
HomePC <oritsesangbemimaminor@gmail.com>
2026-07-03 12:09:49 -0700

    reports end point is ready

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from routers import assistant_routes
+from routers import assistant_routes, predictions_routes,reports_routes
 
 app = FastAPI()
 app.include_router(assistant_routes.router)
+app.include_router(predictions_routes.router)
+app.include_router(reports_routes.router)
 
 @app.get("/")
 def home():
